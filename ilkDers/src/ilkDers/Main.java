2648be096591dc1f0fe97b5cbbfe6e1aaf72b437
Samet Cenk Yücel <83504056+CenkYCL@users.noreply.github.com>
2021-05-02 15:06:16 +0300

    Add files via upload

---
+++ ilkDers/src/ilkDers/Main.java
@@ -0,0 +1,25 @@
+package ilkDers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String mesajKarsilama = ("Merhaba Sayýn Müþterimiz, Dolar Bugün : ");
+		int dolarDun = 7;
+		int dolarBugun = 8;
+		
+		if (dolarDun<dolarBugun) {
+			System.out.println(mesajKarsilama + "Yükseldi");
+		}
+		else if (dolarDun>dolarBugun) {
+			System.out.println(mesajKarsilama + "Düþtü");
+		}
+		else {
+			System.out.println(mesajKarsilama + "Eþit");
+		}
+			
+		
+
+	}
+
+}
