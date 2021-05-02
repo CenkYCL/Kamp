a38391a31097ab0e83bb73a03262f122f753a92f
Samet Cenk Yücel <83504056+CenkYCL@users.noreply.github.com>
2021-05-02 15:26:45 +0300

    Add files via upload

---
+++ forDongusu/src/forDongusu/ForDongusu.java
@@ -0,0 +1,24 @@
+package forDongusu;
+
+public class ForDongusu {
+
+	public static void main(String[] args) {
+		
+		String [] ogrenciler = {
+				"Ali Rýza Ýskender",
+				"Ýlker Yasin Ünal",
+				"Samet Cenk Yücel",
+				"Hatice Cemre Kral",
+				"Ümmügülsüm Yýlmaz",
+				"Veli Yanýk",
+				"Beyza Ekinci",
+				"Yýlmaz Satýcý"
+		};
+				
+			for (String sinif : ogrenciler) {
+				System.out.println(sinif);
+			}
+
+	}
+
+}
