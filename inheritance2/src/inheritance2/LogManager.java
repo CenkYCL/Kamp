98cd2ed56b43d1992e9540cfb9f682ad216d12bb
Samet Cenk Yücel <83504056+CenkYCL@users.noreply.github.com>
2021-05-14 03:30:30 +0300

    Add files via upload

---
+++ inheritance2/src/inheritance2/LogManager.java
@@ -0,0 +1,26 @@
+package inheritance2;
+
+public class LogManager {
+	
+	public void log(int logType) {
+		
+		if (logType ==1 ) {
+			System.out.println("Veritabanýna Loglandý");
+		}
+		
+		else if (logType == 0) {
+			System.out.println("Dosya Loglandý");
+		}
+		
+		else {
+			System.out.println("Email Loglandý");
+		}
+			
+		
+		
+	}
+		
+		
+	}
+/* Database, Files, Mail, */
+
